7c637e71a8072655d91fa8c5cea22aeddcc5a8c0
Michael Chan <Michael.W.Chan@stonybrook.edu>
2021-09-29 01:36:46 -0400

    First Function

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+
 export default class Workspace extends React.Component {
     render() {
+        // console.log(this.props);
+        const {
+            currentList
+        } = this.props;
+        if (currentList){
         return (
             <div id="top5-workspace">
                 <div id="workspace-edit">
@@ -12,8 +18,32 @@ export default class Workspace extends React.Component {
                         <div className="item-number">4.</div>
                         <div className="item-number">5.</div>
                     </div>
+                    <div id="edit-items">
+                        <div className="top5-item">{currentList.items[0]}</div>
+                        <div className="top5-item">{ currentList.items[1]}</div>
+                        <div className="top5-item">{currentList.items[2]}</div>
+                        <div className="top5-item">{currentList.items[3]}</div>
+                        <div className="top5-item">{currentList.items[4]}</div>
+                    </div>
                 </div>
             </div>
         )
+        }
+        else {
+            return (
+                <div id="top5-workspace">
+                <div id="workspace-edit">
+                    <div id="edit-numbering">
+                        <div className="item-number">1.</div>
+                        <div className="item-number">2.</div>
+                        <div className="item-number">3.</div>
+                        <div className="item-number">4.</div>
+                        <div className="item-number">5.</div>
+                    </div>
+                </div>
+                </div>
+
+            )
+        }
     }
 }
